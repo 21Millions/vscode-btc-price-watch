07ae2cf244653f3652963a642624586917143159
陈泽江 <820289461@qq.com>
2019-07-14 15:41:59 +0800

    删除注释

--- src/regCommand.js
+++ src/regCommand.js
@@ -1,9 +1,7 @@
 const vscode = require('vscode');
 const open = require("open");
-const util = require('./../src/util');
 module.exports = function(context) {
     context.subscriptions.push(vscode.commands.registerCommand('coin.focus', (link) => {
-        // vscode.window.showInformationMessage('Hello World！' + link);
         open(link);
     }));
 };
